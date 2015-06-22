f56fcf9af5d34c29b5c43c0a3fcf343f4085fbd5
Jean Marc Delafont <jmdelafont@xykox.net>
2015-06-22 18:04:21 +0200

    Enable blockcart.php override when called by blockcart-ajax.php

--- blockcart-ajax.php
+++ blockcart-ajax.php
@@ -24,7 +24,6 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 // @TODO Find the reason why the blockcart.php is includ multiple time
-include_once(dirname(__FILE__).'/blockcart.php');
 $context = Context::getContext();
-$blockCart = new BlockCart();
+$blockCart = Module::getInstanceByName('blockcart');
 echo $blockCart->hookAjaxCall(array('cookie' => $context->cookie, 'cart' => $context->cart));
