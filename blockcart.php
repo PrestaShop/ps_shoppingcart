bc0708cacb17ed792e72c6e6d98d6b822cce2e94
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-07-15 13:25:51 +0200

    // Version updated to v1.5.9

--- blockcart.php
+++ blockcart.php
@@ -33,7 +33,7 @@ class BlockCart extends Module
 	{
 		$this->name = 'blockcart';
 		$this->tab = 'front_office_features';
-		$this->version = '1.5.8';
+		$this->version = '1.5.9';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
