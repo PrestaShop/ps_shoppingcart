eae61d7e90b0a78ccee5837f10fd9b680fce94cd
djfm <fm.de.jouvencel@gmail.com>
2016-02-09 09:57:43 +0100

    // showModal() can be overridden by the theme

--- blockcart.js
+++ blockcart.js
@@ -1,6 +1,33 @@
 /* global $, prestashop */
 
+/**
+ * This module exposes an extension point in the form of the `showModal` function.
+ *
+ * If you want to override the way the modal window is displayed, simply define:
+ *
+ * prestashop.blockcart = prestashop.blockcart || {};
+ * prestashop.blockcart.showModal = function myOwnShowModal (modalHTML) {
+ *   // your own code
+ *   // please not that it is your responsibility to handle closing the modal too
+ * };
+ *
+ * Attention: your "override" JS needs to be included **before** this file.
+ * The safest way to do so is to place your "override" inside the theme's main JS file.
+ *
+ */
+
 $(document).ready(function () {
+    prestashop.blockcart = prestashop.blockcart || {};
+
+    var showModal = prestashop.blockcart.showModal || function defaultShowModal (modal) {
+        $(document.body).append(modal);
+        $('body').one('click', '#blockcart-modal', function (event) {
+            if (event.target.id === 'blockcart-modal') {
+                $(event.target).remove();
+            }
+        });
+    }
+
     prestashop.on('cart updated', function (event) {
         var refreshURL = $('.blockcart').data('refresh-url');
         var requestData = {};
@@ -19,16 +46,6 @@ $(document).ready(function () {
                 showModal(resp.modal);
             }
         });
-    });
-
-    function showModal (modal) {
-        $(document.body).append(modal);
-    }
-
-    $('body').on('click', '#blockcart-modal', function (event) {
-        if (event.target.id === 'blockcart-modal') {
-            $(event.target).remove();
-        }
     });
 
     $('body').on(
