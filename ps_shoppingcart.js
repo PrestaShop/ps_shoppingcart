28f936e10d5bb2bdac85dbbf53889a694aec1c0a
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2016-08-26 09:23:21 +0200

    FO: Rename events to update cart and shopping cart

--- ps_shoppingcart.js
+++ ps_shoppingcart.js
@@ -17,34 +17,42 @@
  */
 
 $(document).ready(function () {
-    prestashop.blockcart = prestashop.blockcart || {};
+  prestashop.blockcart = prestashop.blockcart || {};
 
-    var showModal = prestashop.blockcart.showModal || function defaultShowModal (modal) {
-        $(document.body).append(modal);
-        $('body').one('click', '#blockcart-modal', function (event) {
-            if (event.target.id === 'blockcart-modal') {
-                $(event.target).remove();
-            }
-        });
-    }
+  var showModal = prestashop.blockcart.showModal || function (modal) {
+    var $body = $('body');
+    $body.append(modal);
+    $body.one('click', '#blockcart-modal', function (event) {
+      if (event.target.id === 'blockcart-modal') {
+        $(event.target).remove();
+      }
+    });
+  };
 
-    prestashop.on('cart updated', function (event) {
+  $(document).ready(function () {
+    prestashop.on(
+      'updateCart',
+      function (event) {
         var refreshURL = $('.blockcart').data('refresh-url');
         var requestData = {};
 
         if (event && event.reason) {
-            requestData = {
-                id_product_attribute: event.reason.idProductAttribute,
-                id_product: event.reason.idProduct,
-                action: event.reason.linkAction
-            };
+          requestData = {
+            id_product_attribute: event.reason.idProductAttribute,
+            id_product: event.reason.idProduct,
+            action: event.reason.linkAction
+          };
         }
 
         $.post(refreshURL, requestData).then(function (resp) {
-            $('.blockcart').replaceWith(resp.preview);
-            if (resp.modal) {
-                showModal(resp.modal);
-            }
+          $('.blockcart').replaceWith(resp.preview);
+          if (resp.modal) {
+            showModal(resp.modal);
+          }
+        }).fail(function (resp) {
+          prestashop.emit('handleError', {eventType: 'updateShoppingCart', resp: resp});
         });
-    });
+      }
+    );
+  });
 });
