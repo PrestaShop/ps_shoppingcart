e75e847303d529bcaf72d34f6ad90e5c8b2599f0
Maxime Biloé <maxime.biloe@gmail.com>
2017-01-02 17:37:36 +0100

    // Version updated to v1.1.1

--- ps_shoppingcart.php
+++ ps_shoppingcart.php
@@ -38,7 +38,7 @@ class Ps_Shoppingcart extends Module implements WidgetInterface
     {
         $this->name = 'ps_shoppingcart';
         $this->tab = 'front_office_features';
-        $this->version = '1.1.0';
+        $this->version = '1.1.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
