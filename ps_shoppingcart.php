a25b076f817e132c977cee6d42faef902b2072f8
MathiasReker <mathias@reker.dk>
2018-12-30 22:44:58 +0100

    Multiline whitespace before semicolons

--- ps_shoppingcart.php
+++ ps_shoppingcart.php
@@ -138,8 +138,7 @@ class Ps_Shoppingcart extends Module implements WidgetInterface
             parent::install()
                 && $this->registerHook('header')
                 && $this->registerHook('displayTop')
-                && Configuration::updateValue('PS_BLOCK_CART_AJAX', 1)
-        ;
+                && Configuration::updateValue('PS_BLOCK_CART_AJAX', 1);
     }
 
     public function renderForm()
