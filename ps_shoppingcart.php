227536fe657c244b8a9e363400c4ac9fd747294b
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2023-02-03 14:45:37 +0100

    Update ps_shoppingcart.php

--- ps_shoppingcart.php
+++ ps_shoppingcart.php
@@ -37,7 +37,7 @@ class Ps_Shoppingcart extends Module implements WidgetInterface
     {
         $this->name = 'ps_shoppingcart';
         $this->tab = 'front_office_features';
-        $this->version = '2.0.6';
+        $this->version = '2.0.7';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
